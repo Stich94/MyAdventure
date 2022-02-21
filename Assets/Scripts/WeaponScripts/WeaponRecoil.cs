d76e1368e4ff68197f74ca3580d6f752632ba450
BernhardW <bernhard.woltron@gmail.com>
2022-02-21 16:15:29 +0100

    Recoil controlled with Imulse
    Cinemachine Impulse - Noise Controls

--- Assets/Scripts/WeaponScripts/WeaponRecoil.cs
+++ Assets/Scripts/WeaponScripts/WeaponRecoil.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class WeaponRecoil : MonoBehaviour
@@ -9,24 +10,31 @@ public class WeaponRecoil : MonoBehaviour
     [SerializeField] CinemachineVirtualCamera playerCam;
     public CinemachineVirtualCamera PlayerCamera { get { return playerCam; } set { playerCam = value; } }
 
-    [SerializeField] float verticalRecoil = 0.01f;
+    [SerializeField] float verticalRecoilAmount = 0.01f;
     [SerializeField] float duration;
-    float time;
-    Vector2 aimDir;
 
+    CinemachineImpulseSource source;
 
+    private void Start()
+    {
+        source = GetComponent<CinemachineImpulseSource>();
+    }
     private void Update()
     {
-        if (time > 0)
-        {
-            playerCam.gameObject.transform.rotation = Quaternion.Euler(playerCam.transform.position.x - (verticalRecoil * Time.deltaTime) / duration, 0f, 0f);
-            time -= Time.deltaTime;
-        }
+        // if (time > 0)
+        // {
+        //     playerCam.gameObject.transform.rotation = Quaternion.Euler(playerCam.transform.position.x - (verticalRecoil * Time.deltaTime) / duration, 0f, 0f);
+        //     time -= Time.deltaTime;
+        // }
+
+
     }
     public void GenerateRecoil()
     {
-        time = duration;
+        // source.GenerateImpulse(Camera.main.transform.right);
+        source.GenerateImpulse(Camera.main.transform.forward);
     }
+
 
 }
 
